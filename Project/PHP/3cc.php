a124767340d3c4093effdf18424e82c2302df3fd
Meghana Kiran <meghanakiran.mk@gmail.com>
2019-10-07 23:58:56 -0500

    Update 3cc.php

--- Project/PHP/3cc.php
+++ Project/PHP/3cc.php
@@ -8,7 +8,7 @@ $select=$_POST['field'];
 //Execute the query
  
   //connection 
-$con=mysqli_connect('cs.okstate.edu','mkiran', 'galena8antics4reveal','mkiran')
+$con=mysqli_connect('******','******', '******','******')
 or die('error connecting to database');  //connect to database 
 
 $query = "ALTER TABLE Orderdetails ADD $select int"; //query
