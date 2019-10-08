57d28733e3a3ed535046a1302fd8cf44bccc31f6
Meghana Kiran <meghanakiran.mk@gmail.com>
2019-10-08 00:09:31 -0500

    Update 7.php

--- Project/PHP/7.php
+++ Project/PHP/7.php
@@ -3,10 +3,10 @@
 $select=$_POST['pname'];
   
 
-$servername = "cs.okstate.edu";
-$username = "mkiran";
-$password = "galena8antics4reveal";
-$dbname = "mkiran";
+$servername = "****";
+$username = "****";
+$password = "****";
+$dbname = "****";
 
 // Create connection 
 $conn = mysqli_connect($servername, $username, $password, $dbname); 
