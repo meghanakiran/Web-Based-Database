5e1ac13d818fa616a118f34623b42ad03b36288e
Meghana Kiran <meghanakiran.mk@gmail.com>
2019-10-07 23:52:08 -0500

    Update 1c.php

--- Project/PHP/1c.php
+++ Project/PHP/1c.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "cs.okstate.edu";
-$username = "mkiran";
-$password = "galena8antics4reveal";
-$dbname = "mkiran";
+$servername = "*******";
+$username = "******";
+$password = "******";
+$dbname = "******";
 
 // Create connection 
 $conn = mysqli_connect($servername, $username, $password, $dbname); 
